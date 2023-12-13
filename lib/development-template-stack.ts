17aebd82a9328bea5e7c0a7db98616efb56ecc62
Tomoaki Imai <tomoima525@gmail.com>
2023-12-12 21:48:46 -0800

    clean!!

--- lib/development-template-stack.ts
+++ lib/development-template-stack.ts
@@ -12,12 +12,26 @@ import * as path from "path";
 export class DevelopmentTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    const llmLambea = new lambda_nodejs.NodejsFunction(this, "simple-llm", {
+
+    const llmLayer = new lambda.LayerVersion(this, "llmLayer", {
+      compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
+      code: lambda.Code.fromAsset(
+        path.join(`${__dirname}/../`, "layers/llm-layer"),
+      ),
+      description: "Langchain LLM Layer",
+    });
+
+    const llmLambda = new lambda_nodejs.NodejsFunction(this, "simple-llm", {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: "handler",
       entry: path.join(`${__dirname}/../`, "functions", "simple-llm/index.ts"),
       environment: {
         OPENAI_API_KEY: "openai-api-key",
+      },
+      layers: [llmLayer],
+      bundling: {
+        externalModules: ["langchain"],
+        format: lambda_nodejs.OutputFormat.ESM,
       },
       memorySize: 256,
       timeout: Duration.seconds(120),
